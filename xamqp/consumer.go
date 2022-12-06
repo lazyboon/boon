b34e849ff8c39a90893daba5a2440686471a8701
lazyboon <>
2022-12-06 14:52:03 +0800

    xamqp

--- xamqp/consumer.go
+++ xamqp/consumer.go
@@ -1,53 +1,5 @@
 package xamqp
 
-import "github.com/streadway/amqp"
-
-type HandlerType int8
-
-const (
-	HandlerTypeAck HandlerType = iota
-	HandlerTypeReject
-)
-
-type IHandlerResponse interface {
-	Type() HandlerType
-	Param() bool
-}
-
-type handlerAck struct {
-	multiple bool
-}
-
-func NewHandlerAck(multiple bool) *handlerAck {
-	return &handlerAck{multiple: multiple}
-}
-
-func (h *handlerAck) Type() HandlerType {
-	return HandlerTypeAck
-}
-
-func (h *handlerAck) Param() bool {
-	return h.multiple
-}
-
-type handlerReject struct {
-	requeue bool
-}
-
-func NewHandlerReject(requeue bool) *handlerReject {
-	return &handlerReject{requeue: requeue}
-}
-
-func (h *handlerReject) Type() HandlerType {
-	return HandlerTypeReject
-}
-
-func (h *handlerReject) Param() bool {
-	return h.requeue
-}
-
-type Handler func(delivery *amqp.Delivery) IHandlerResponse
-
 type Consumer struct {
 	Queue     string
 	Tag       string
@@ -56,5 +8,4 @@ type Consumer struct {
 	NoLocal   bool
 	NoWait    bool
 	Args      map[string]interface{}
-	Handler   Handler
 }
